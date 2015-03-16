b0de8e6923f6ff5162819395764280accee5fc2f
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-03-16 02:28:27 -0700

    cleaned errors and new expression

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,14 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /move = move || "getInput()"/;
+    return move = move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /move = move || "randomPlay()"/;
+    return move === randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
@@ -40,21 +40,22 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /if (playerMove === "rock") {
+    if (playerMove === "rock") {
         if (computerMove === "scissors") {
             winner = "player";}
     }   if (computerMove === "paper") {
             winner = "computer";
     }   if (computerMove === "rock")  {
-            winner = "tie";}
+            winner = "tie";
+        }
 
     if (playerMove === "scissors" ) {
         if (computerMove === "paper") {
             winner = "player"; 
-    }   if ( computerMove = "rock") {
+    }   if (computerMove === "rock") {
             winner = "computer";
     }   if (computerMove === "scissors"){
-            winner = "tie"
+            winner = "tie";
     
     } if (playerMove === "paper" ) {
         if (computerMove === "rock") {
@@ -65,8 +66,9 @@ function getWinner(playerMove,computerMove) {
             winner = "tie";
     }
         
-    }    /
+    }    
     return winner;
+}
 }
 
 function playToFive() {
@@ -90,8 +92,18 @@ function playToFive() {
 7. After the winner has been decided, I need the computer to then proclaim who was the winner of the match and to spout to restart the loop.
     */
     while (playerWins < 5) || (computerWins < 5) {
-         return "getInput";
-    } /
+         getInput();
+         randomPlay();
+         getPlayerMove();
+         getComputerMove();
+         getWinner();
+         if ("player") {
+            (playerWins += 1);
+         }
+         else if ("computer") {
+            (computerWins += 1);
+         }
+    } 
     return [playerWins, computerWins];
 }
 
